f4f0ab82208d16cc1f7bdabd079d1f0491e31b66
lygl25 <lygl25@163.com>
2020-05-14 17:24:32 +0800

    增加场景切换

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import Phaser from "phaser"
-import ImgStart from "./assets/images/TempleHallForest.jpg"
+import ImgBackground from "./assets/images/TempleHallForest.jpg"
 import ImgGround from  './assets/images/platform.png'
 import ImgStar from  './assets/images/star.png'
 import ImgBomb from  './assets/images/bomb.png'
 import ImgDude from  './assets/images/dude.png'
+import ImgLongarrow from  './assets/images/longarrow.png'
 
 
-console.log(Phaser);
 // 使用 Web Audio API 
 var audioContext;
 try {
@@ -15,132 +15,149 @@ try {
     console.error(e);
 }
 
-const config = {
-  title:"我的Phaser3演示代码x",
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 1366,
-  height: 768,
-  //静态物体属性
-  physics: {
-            //设置物理模式
-            default: 'arcade',
-            arcade: {
-                gravity: { y: 300 },
-                debug: false
-            }
-        },
-  scene: {
-    preload: preload,
-    create: create,
-    update:update
+//游戏开始菜单
+var SceneA = new Phaser.Class({
+
+  Extends: Phaser.Scene,
+
+  initialize:
+
+  function SceneB ()
+  {
+      Phaser.Scene.call(this, 'sceneA');
+
+      this.pic;
   },
-  //游戏画面显示比例设置
-  scale: {
-    mode: Phaser.Scale.FIT,//自动缩放游戏画面
-    autoCenter: Phaser.Scale.CENTER_BOTH//游戏画面居中显示
 
-},
-//声音配置
-audio: {
- context: audioContext
-}
-}
-  
-const game = new Phaser.Game(config)
-var   platforms //平台
-var   player    //精灵
-var   cursors   //控制
-var   stars     //星星
-var   score=0   //分数
-var   scoreText //分数名称
-var   bombs     //炸弹
-var   gameOver = false
-var   audioDiamond//精灵碰撞星星的声音
-var   audioJump//精灵跳跃声音
-var   audioDeath//精灵碰撞炸弹死亡声音
-
-function preload() {
-  console.log("读取资源开始")
-  console.log(ImgStart)
-  this.load.image('start', ImgStart)//背景图
-  this.load.image('ground', ImgGround)//平台图
-  this.load.image('star', ImgStar)//星星
-  this.load.image('bomb', ImgBomb)//炸弹
-  this.load.spritesheet('dude', ImgDude, { frameWidth: 32, frameHeight: 48 })//精灵表方式载入精灵图片
-  this.load.audio('audioDiamond', ['static/Diamond.mp3']);//载入精灵碰撞星星的声音
-  this.load.audio('audioJump', ['static/Jump.mp3']);//载入跳跃声音
-  this.load.audio('audioDeath', ['static/Death.mp3']);//载入精灵碰撞炸弹死亡声音
-  console.log("读取资源结束")
-  
-}
-
-
-function create() {
-      this.add.image(683, 384,'start')//背景图
-      platforms = this.physics.add.staticGroup()//创建静态物体组件，这里是地面和平台，staticGroup静体组
-      platforms.create(683,717.76, 'ground').setScale(3.415).refreshBody()//地面
-      platforms.create(600, 550, 'ground')
-      platforms.create(200, 450, 'ground')
-      platforms.create(966, 350, 'ground').setScale(2,1).refreshBody()//最上面的平台
-
+  preload: function ()
+  {
+      this.load.image('arrow', ImgLongarrow);
+      console.log("读取资源开始")
+    
+      this.load.image('background', ImgBackground)//背景图
+      this.load.image('ground', ImgGround)//平台图
+      this.load.image('star', ImgStar)//星星
+      this.load.image('bomb', ImgBomb)//炸弹
+      this.load.spritesheet('dude', ImgDude, { frameWidth: 32, frameHeight: 48 })//精灵表方式载入精灵图片
+      this.load.audio('audioDiamond', ['static/Diamond.mp3']);//载入精灵碰撞星星的声音
+      this.load.audio('audioJump', ['static/Jump.mp3']);//载入跳跃声音
+      this.load.audio('audioDeath', ['static/Death.mp3']);//载入精灵碰撞炸弹死亡声音
+      console.log("读取资源结束")
      
-      player = this.physics.add.sprite(100, 150, 'dude')//创建一个精灵
-      
-      audioDiamond = this.sound.add('audioDiamond', {volume: 0.5});//创建精灵碰撞星星声音
-      audioJump = this.sound.add('audioJump', {volume: 0.5});//创建精灵碰撞星星声音
-      audioDeath = this.sound.add('audioDeath', {volume: 0.5});//创建精灵碰撞星星声音
-      
-      player.body.setGravityY(300)
-      player.setBounce(0.2)//精灵的弹力值
-      player.setCollideWorldBounds(true)
-     
-      //向左跑动画
-      this.anims.create({
-          key: 'left',
-          frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-          frameRate: 10,
-          repeat: -1
-      })
-      //停止跑动的图片
-      this.anims.create({
-          key: 'turn',
-          frames: [ { key: 'dude', frame: 4 } ],
-          frameRate: 20
-      })
+      console.log(game.config.width)
+  },
 
-      //向右跑动画
-      this.anims.create({
-          key: 'right',
-          frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-          frameRate: 10,
-          repeat: -1
-      })
-      //载入星星，group是动体组
-      stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,//星星数量，会自动创建一个 后重复11次创建，总共会创建12个星星
-        setXY: { x: 12, y: 0, stepX: 70 }
-    })
+  create: function ()
+  {
+      this.pic = this.add.image(game.config.width/2,game.config.height/2, 'arrow').setOrigin(0, 0.5);
 
-        //炸弹  
-        bombs = this.physics.add.group()
+      this.input.once('pointerdown', function () {
 
+          this.scene.switch('sceneB');
 
+      }, this);
+  },
+
+  wake: function ()
+  {
+      this.input.once('pointerdown', function () {
+
+          this.scene.switch('sceneB');
+
+      }, this);
+  },
+
+  update: function (time, delta)
+  {
+      this.pic.rotation += 0.01;
+  }
+
+});
+
+//游戏内容
+var SceneB = new Phaser.Class({
+
+  Extends: Phaser.Scene,
+
+  initialize:
+
+  function SceneB ()
+  {
+      Phaser.Scene.call(this, { key: 'sceneB' });
+
+      this.face;
+  },
+
+  preload: function ()
+  {
+ 
+
+  },
+
+  create: function ()
+  {
+          this.add.image(683, 384,'background')//背景图
+        platforms = this.physics.add.staticGroup()//创建静态物体组件，这里是地面和平台，staticGroup静体组
+        platforms.create(683,717.76, 'ground').setScale(3.415).refreshBody()//地面
+        platforms.create(600, 550, 'ground')
+        platforms.create(200, 450, 'ground')
+        platforms.create(966, 350, 'ground').setScale(2,1).refreshBody()//最上面的平台
+  
+       
+        player = this.physics.add.sprite(100, 150, 'dude')//创建一个精灵
         
-      scoreText=this.add.text(16,16,'score:0',{fontSize:'32px',fill:'#FFF'});//计分初试显示
-      this.physics.add.collider(player, platforms)//角色和平台的碰撞
-      this.physics.add.collider(stars, platforms)//星星和平台的碰撞
-      this.physics.add.collider(bombs, platforms);//炸弹和平台的碰撞
-      
-      this.physics.add.overlap(player, stars, collectStar, null, this) //角色和星星的碰撞
-      this.physics.add.collider(player, bombs, hitBomb, null, this); //角色和炸弹的碰撞
-      cursors = this.input.keyboard.createCursorKeys()//键盘控制
-      ariseBomb()//掉落一个炸弹
-
-}
-
-function update(){
+        audioDiamond = this.sound.add('audioDiamond', {volume: 0.5});//创建精灵碰撞星星声音
+        audioJump = this.sound.add('audioJump', {volume: 0.5});//创建精灵碰撞星星声音
+        audioDeath = this.sound.add('audioDeath', {volume: 0.5});//创建精灵碰撞星星声音
+        
+        player.body.setGravityY(300)
+        player.setBounce(0.2)//精灵的弹力值
+        player.setCollideWorldBounds(true)
+       
+        //向左跑动画
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        })
+        //停止跑动的图片
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        })
+  
+        //向右跑动画
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        })
+        //载入星星，group是动体组
+        stars = this.physics.add.group({
+          key: 'star',
+          repeat: 11,//星星数量，会自动创建一个 后重复11次创建，总共会创建12个星星
+          setXY: { x: 12, y: 0, stepX: 70 }
+      })
+  
+          //炸弹  
+          bombs = this.physics.add.group()
+  
+  
+          
+        scoreText=this.add.text(16,16,'score:0',{fontSize:'32px',fill:'#FFF'});//计分初试显示
+        this.physics.add.collider(player, platforms)//角色和平台的碰撞
+        this.physics.add.collider(stars, platforms)//星星和平台的碰撞
+        this.physics.add.collider(bombs, platforms);//炸弹和平台的碰撞
+        
+        this.physics.add.overlap(player, stars, collectStar, null, this) //角色和星星的碰撞
+        this.physics.add.collider(player, bombs, hitBomb, null, this); //角色和炸弹的碰撞
+        cursors = this.input.keyboard.createCursorKeys()//键盘控制
+        ariseBomb()//掉落一个炸弹
+  },
+  update:function (){
     if (gameOver)
     {
         return;
@@ -174,7 +191,57 @@ function update(){
           audioJump.play()//播放跳跃声音
       }
 
+
+  }
+
+
+
+});
+
+
+const config = {
+  title:"我的Phaser3演示代码x",
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 1366,
+  height: 768,
+  //静态物体属性
+  physics: {
+            //设置物理模式
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+        },
+  scene: [ SceneA,SceneB],
+  //游戏画面显示比例设置
+  scale: {
+    mode: Phaser.Scale.FIT,//自动缩放游戏画面
+    autoCenter: Phaser.Scale.CENTER_BOTH//游戏画面居中显示
+
+},
+//声音配置
+audio: {
+ context: audioContext
 }
+}
+  
+const game = new Phaser.Game(config)
+var   platforms //平台
+var   player    //精灵
+var   cursors   //控制
+var   stars     //星星
+var   score=0   //分数
+var   scoreText //分数名称
+var   bombs     //炸弹
+var   gameOver = false
+var   audioDiamond//精灵碰撞星星的声音
+var   audioJump//精灵跳跃声音
+var   audioDeath//精灵碰撞炸弹死亡声音
+
+
+
 
 //精灵和星星碰撞的处理
 function collectStar (player, star)
@@ -228,46 +295,6 @@ function ariseBomb(){
 
 
 
-// var SceneA = new Phaser.Class({
 
-//   Extends: Phaser.Scene,
-
-//   initialize:
-
-//   function SceneA ()
-//   {
-//       Phaser.Scene.call(this, { key: 'sceneA' });
-
-//       this.face;
-//   },
-
-//   preload: function ()
-//   {
-//       this.load.image('face', 'assets/pics/bw-face.png');
-//   },
-
-//   create: function ()
-//   {
-//       this.face = this.add.image(0, 0, 'face').setOrigin(0);
-
-//       this.input.once('pointerdown', function () {
-
-//           this.scene.transition({
-//               target: 'sceneB',
-//               duration: 2000,
-//               moveBelow: true,
-//               onUpdate: this.transitionOut,
-//               data: { x: 400, y: 300 }
-//           });
-
-//       }, this);
-//   },
-
-//   transitionOut: function (progress)
-//   {
-//       this.face.y = (600 * progress);
-//   }
-
-// });
 
 
